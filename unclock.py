fcad368635bd8e42677f6b5e262f4d78e173b5f8
Aithel Christo <christoaithel@gmail.com>
2024-11-01 18:40:24 +0530

    appinit

---
+++ unclock.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from datetime import datetime
+import pytz
+import random
+
+st.title("UnClock App - The Useless Clock App")
+st.sidebar.title("UnClock Features")
+
+# Feature selection
+feature = st.sidebar.radio("Choose a Feature", ("World Clock", "Stopwatch", "Timer", "Alarm"))
+
+if feature == "World Clock":
+    st.header("World Clock")
+    # Code for World Clock will go here
+
+elif feature == "Stopwatch":
+    st.header("Stopwatch")
+    # Code for Stopwatch will go here
+
+elif feature == "Timer":
+    st.header("Timer")
+    # Code for Timer will go here
+
+elif feature == "Alarm":
+    st.header("Alarm")
+    # Code for Alarm will go here
